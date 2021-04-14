d1638f52d72b9e508195e8842eb417e8de8e0772
Prakash <prakashchater@gmail.com>
2021-04-15 00:03:51 +0530

    15/04april

---
+++ Fightingthedarkness.py
@@ -0,0 +1,19 @@
+# def darkness(arr):
+#     count = 0
+#     temp = []
+#     i = 0
+#     while i < len(arr):
+#         day = arr[i] - 1
+#         temp.append(day)
+#         i += 1
+#         count += 1
+#         if sum(temp) != 0:
+#             return temp
+#
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     arr = [1,1,2]
+#     darkness(arr)
--- rotate_clockwise.py
+++ rotate_clockwise.py
@@ -23,35 +23,45 @@
 #     n = len(arr)
 #     multiplyleftright(arr,n)
 
+# def rotate(arr, d, n):
+#     temp = []
+#     i = 0
+#     while (i < d):
+#         temp.append(arr[i])
+#         i = i + 1
+#     i = 0
+#     while (d < n):
+#         arr[i] = arr[d]
+#         i = i + 1
+#         d = d + 1
+#     arr[:] = arr[: i] + temp
+#     return arr
+#
+# if __name__ == '__main__':
+#     for i in range(int(input())):
+#         a = list(map(int, input().split()))
+#         d = a[1]
+#         n = a[0]
+#         arr = list(map(int,input().split()))
+#         print(rotate(arr,d,n))
 
-# T = int(input())
-# for i in range(T):
-#     a = list(map(int,input().split()))
-#     n = a[0]
-#     d = a[1]
-#     arr = list(map(int, input().split()))
-#     arr[:] = arr[d:n]+arr[0:d]
-#     print(arr)
-
-def rotate(arr, d, n):
-    temp = []
-    i = 0
-    while (i < d):
-        temp.append(arr[i])
-        i = i + 1
-    i = 0
-    while (d < n):
-        arr[i] = arr[d]
-        i = i + 1
-        d = d + 1
-    arr[:] = arr[: i] + temp
+def rotation(arr,d,n):
+    for i in range(0,d):
+        temp = arr[0]
+        for j in range(0,n-1):
+            arr[j] = arr[j+1]
+        arr[n-1] = temp
     return arr
 
 if __name__ == '__main__':
-    for i in range(int(input())):
-        a = list(map(int, input().split()))
-        d = a[1]
+    T = int(input())
+    for i in range(T):
+        a = list(map(int,input().split()))
         n = a[0]
+        d = a[1]
         arr = list(map(int,input().split()))
-        print(rotate(arr,d,n))
+        print(rotation(arr, d, n))
 
+    # [29, 42, 51, 94, 1, 35, 65, 25, 40, 13, 27, 87, 95, 40, 96, 71, 35, 79, 68, 2, 98, 3, 18, 93, 53, 57, 2, 81, 87, 42,
+    #  66, 90, 45, 20, 41, 30, 32, 18, 98, 72, 82, 76, 10, 28, 68, 57, 98, 54, 87, 66, 7, 84, 20, 25, 29, 72, 33, 30, 4,
+    #  20, 71, 69, 9, 16, 41, 50, 97, 24, 19, 46, 47, 52, 22, 56, 80, 89, 65]
