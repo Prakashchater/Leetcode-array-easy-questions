d0f94bebc79a57d0ac1f9066c30ec00a6e3f857f
Prakash <prakashchater@gmail.com>
2021-04-18 23:38:50 +0530

    18/04april

--- TransposeMatrix.py
+++ TransposeMatrix.py
@@ -1,12 +1,30 @@
-def transpose(arr):
-    res = []
-    for i in range(len(arr[0])):
-        temp = []
-        for j in range(len(arr)):
-            temp.append(arr[j][i])
-        res.append(temp)
-    return res
+# def transpose(arr):
+#     res = []
+#     for i in range(len(arr[0])):
+#         temp = []
+#         for j in range(len(arr)):
+#             temp.append(arr[j][i])
+#         res.append(temp)
+#     return res
+#
+# if __name__ == '__main__':
+#     arr = [[1,2,3],[4,5,6]]
+#     print(transpose(arr))
+
+
+def addMatrices(x,y):
+    l = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(len(x[0])):
+        for j in range(len(y[0])):
+            l[i][j] = x[i][j] + y[i][j]
+    return l
+
 
 if __name__ == '__main__':
-    arr = [[1,2,3],[4,5,6]]
-    print(transpose(arr))
+    x = [[1,2,1],
+    [4 ,5,6],
+    [7 ,8,9]]
+    y = [[9,8,7],
+    [6,5,4],
+    [3,2,1]]
+    print(addMatrices(x,y))
---
+++ matrics.py
@@ -0,0 +1,17 @@
+# def multiMatrix(x, y):
+#     l = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+#     for i in range(len(x)):
+#         for j in range(len(y[0])):
+#             for k in range(len(y)):
+#                 print(k)
+#                 l[i][j] += x[i][k] * y[k][j]
+#     return l
+# if __name__ == '__main__':
+#     x = [[1, 7, 3],
+#          [3, 5, 6],
+#          [6, 8, 9]]
+#     y = [[1, 1, 1, 2],
+#          [6, 7, 3, 0],
+#          [4, 5, 9, 1]]
+#     print(multiMatrix(x, y))
+
