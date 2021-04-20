176d000f0ce79424ad002c51ae3ae961ed895610
Prakash <prakashchater@gmail.com>
2021-04-21 00:00:52 +0530

    commit

--- DefuseTheBomb.py
+++ DefuseTheBomb.py
@@ -1,4 +1,25 @@
-arr = [1,2,3,4]
-n = len(arr)
-res = [0]*n
-print(res)
+def defuse(arr, k):
+    n = len(arr)
+    res = [0] * n
+    for i in range(n):
+        if k > 0:
+            total = 0
+            for x in range(1, k+1):
+                total += arr[(i+x) % n]
+            res[i] = total
+        elif k < 0:
+            total = 0
+            for x in range(1,-k+1):
+                total += arr[(i-x) % n]
+            res[i] = total
+        else:
+            res[i] = 0
+    return res
+
+
+
+
+if __name__ == '__main__':
+    arr = [5, 7, 1, 4]
+    k = 3
+    print(defuse(arr,k))
