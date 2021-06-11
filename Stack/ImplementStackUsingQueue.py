63a55576b08279e0f8d36367a699391de59a128c
Prakash <prakashchater@gmail.com>
2021-06-11 23:09:02 +0530

    practice

---
+++ Stack/ImplementStackUsingQueue.py
@@ -0,0 +1,22 @@
+import collections
+
+
+class Stack:
+    def __init__(self):
+        self.stack = collections.deque
+
+    def push(self, x):
+        return self.stack.append(x)
+
+    def pop(self):
+        for i in range(len(self.stack)-1):
+            self.stack.append(self.stack.popleft())
+        return self.stack.popleft()
+
+    def top(self):
+        return self.stack[-1]
+
+    def empty(self):
+        return len(self.stack) == 0
+
+
