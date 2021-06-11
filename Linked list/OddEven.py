53e8921af8b0cda6890e761ab7da0411fd4ba894
Prakash <prakashchater@gmail.com>
2021-06-11 20:28:41 +0530

    practice

--- Linked list/OddEven.py
+++ Linked list/OddEven.py
@@ -1,23 +1,55 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#
+# def oddEvenList(self, head):
+#     odds = Node(0)
+#     evens = Node(0)
+#     oddsHead = odds
+#     evensHead = evens
+#     isOdd = True
+#     while head:
+#         if isOdd:
+#             odds.next = head
+#             odds = odds.next
+#         else:
+#             evens.next = head
+#             evens = evens.next
+#         isOdd = not isOdd
+#         head = head.next
+#     evens.next = None
+#     odds.next = evensHead.next
+#     return oddsHead.next
+
+
 
 def oddEvenList(self, head):
-    odds = Node(0)
-    evens = Node(0)
-    oddsHead = odds
-    evensHead = evens
-    isOdd = True
-    while head:
-        if isOdd:
-            odds.next = head
-            odds = odds.next
+    if head is None or head.next is None:
+        return head
+
+    odd_p = head
+    even_p = head.next
+    odd = odd_p
+    even = even_p
+    while even is not None:
+        if even.next is not None:
+            odd.next = even.next
         else:
-            evens.next = head
-            evens = evens.next
-        isOdd = not isOdd
-        head = head.next
-    evens.next = None
-    odds.next = evensHead.next
-    return oddsHead.next
+            odd.next = even_p
+            return odd_p
+        odd = odd.next
+        even.next = odd.next
+        even = even.next
+
+    odd.next = even_p
+    return odd
+
+
+
+
+
+
+
+
+
