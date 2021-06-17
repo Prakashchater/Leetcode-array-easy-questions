46006d01ebe12d99f1ce03699a3f89ed4fe56585
Prakash <prakashchater@gmail.com>
2021-06-18 00:03:10 +0530

    practice!

--- MergeInterval.py
+++ MergeInterval.py
@@ -22,14 +22,6 @@ def mergeInterval(arr):
         print(m[i], end=" ")
 """
 def mergeInterval(arr):
-    # arr.sort()
-    # m = []
-    # for i in arr:
-    #     if not m or m[-1][-1] < i[0]:
-    #         m.append(i)
-    #     else:
-    #         m[-1][-1] = max(m[-1][-1], i[1])
-    # return m
     arr.sort()
     m = []
     for i in arr:
