82d519a508cc1bdc59a4e3055b0d3b2ec674541f
Prakash <prakashchater@gmail.com>
2021-06-20 23:15:44 +0530

    practice!

--- CommonElement.py
+++ CommonElement.py
@@ -2,14 +2,15 @@
 def findCommon(A, B, C, n1, n2, n3):
     # Initialize starting indexes for ar1[], ar2[] and ar3[]
     i, j, k = 0, 0, 0
-
+    ans = []
+    res = []
     # Iterate through three arrays while all arrays have elements
     while (i < n1 and j < n2 and k < n3):
 
         # If x = y and y = z, print any of them and move ahead
         # in all arrays
         if (A[i] == B[j] and B[j] == C[k]):
-            print(A[i])
+            ans.append(A[i])
             i += 1
             j += 1
             k += 1
@@ -26,13 +27,18 @@ def findCommon(A, B, C, n1, n2, n3):
         else:
             k += 1
 
+    for i in ans:
+        if i not in res:
+            res.append(i)
+    return res
+
 
 # Driver program to check above function
-A = [1, 5, 10, 20, 40, 80]
-B = [6, 7, 20, 80, 100]
-C = [3, 4, 15, 20, 30, 70, 80, 120]
+A = [3,3,3]
+B = [3,3,3]
+C = [3,3,3]
 n1 = len(A)
 n2 = len(B)
 n3 = len(C)
-findCommon(A, B, C, n1, n2, n3)
+print(findCommon(A, B, C, n1, n2, n3))
 
